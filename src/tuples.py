0858111ec4907d07e88d045d7d870d684dcd808d
Kathryn Hampton <khampton119@gmail.com>
2019-02-20 19:09:37 -0500

    completed comp, file, slices and tuples

--- src/tuples.py
+++ src/tuples.py
@@ -31,14 +31,19 @@ b = (-14, 72)
 print("Distance is: {:.2f}".format(dist(a, b)))
 
 
-
 # Write a function `print_tuple` that prints all the values in a tuple
 
 # YOUR CODE HERE
 
 t = (1, 2, 5, 7, 99)
-print_tuple(t)  # Prints 1 2 5 7 99, one per line
+# Prints 1 2 5 7 99, one per line
+def print_tuple(tuple):
+    for i in tuple:
+      print(i)
+print_tuple(t)
+
 
 # Declare a tuple of 1 element then print it
-u = (1)  # What needs to be added to make this work?
+u = (1,)  # What needs to be added to make this work?
 print_tuple(u)
+
